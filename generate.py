df369d6c4243e372abb2924e6ca443b34ea693b4
lisovskey <ritofzeed@gmail.com>
2018-11-15 02:43:49 +0300

    fix text generation for embedding

--- generate.py
+++ generate.py
@@ -1,28 +1,26 @@
 import numpy as np
 
 
-def sample(preds, diffusion):
-    """
-    Make a diffusion of chars predictions and take most possible.
-    """
-    preds = [pred + np.random.uniform(high=diffusion) for pred in preds]
-    return np.argmax(preds)
-
-
 def sample_text(model, length, chars_indices, indices_chars, sequence,
                 diffusion):
     """
     Yield predicted char after `sequence`.
     Shift `sequence` one char further `length` times.
     """
-    x_pred = np.zeros([1, len(sequence), len(chars_indices)])
+    def sample(preds, diffusion):
+        """
+        Make a diffusion of chars predictions and take most possible.
+        """
+        preds = [pred + np.random.uniform(high=diffusion) for pred in preds]
+        return np.argmax(preds)
+
+    x_pred = np.zeros([1, len(sequence)])
     for i, char in enumerate(sequence):
-        x_pred[0, i, chars_indices[char]] = 1.0
+        x_pred[0, i] = chars_indices[char]
     for _ in range(length):
         preds = model.predict(x_pred, verbose=0)[0]
         next_index = sample(preds, diffusion)
         next_char = indices_chars[next_index]
         yield next_char
         x_pred = np.roll(x_pred, -1, axis=1)
-        x_pred[0, -1] = np.zeros(len(chars_indices))
-        x_pred[0, -1, chars_indices[next_char]] = 1.0
+        x_pred[0, -1] = chars_indices[next_char]
