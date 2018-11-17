fix sample_text, average loss, explicit hidden init

generate.py
@@ -3,28 +3,20 @@ import torch
 
 
 def sample_text(model, length, chars_indices, indices_chars, sequence,
-                diffusion, use_cuda=False):
+                temperature):
     """
     Yield predicted char after `sequence`.
     Shift `sequence` one char further `length` times.
     """
-    def sample(preds, diffusion):
-        """
-        Make a diffusion of chars predictions and take most possible.
-        """
-        preds = [pred + np.random.uniform(high=diffusion) for pred in preds]
-        return np.argmax(preds)
-
-    x_pred = np.zeros([1, len(sequence)])
-    for i, char in enumerate(sequence):
-        x_pred[0, i] = chars_indices[char]
-    for _ in range(length):
-        x_pred = torch.tensor(x_pred, dtype=torch.long)
-        if use_cuda:
-            x_pred = x_pred.cuda()
-        preds = model(x_pred, use_cuda)[0]
-        next_index = sample(preds, diffusion)
-        next_char = indices_chars[next_index]
-        yield next_char
-        x_pred = np.roll(x_pred, -1, axis=1)
-        x_pred[0, -1] = chars_indices[next_char]
+    model.init_hidden(1)
+    weight = next(model.parameters())
+    x_pred = [[chars_indices[char] for char in sequence]]
+    x_pred = weight.new_tensor(x_pred, dtype=torch.long)
+    with torch.no_grad():
+        for _ in range(length):
+            preds = model(x_pred).squeeze()
+            char_weights = preds.div(temperature).exp()
+            next_index = torch.multinomial(char_weights, 1).squeeze().item()
+            yield indices_chars[next_index]
+            x_pred = torch.cat([x_pred[:, 1:], x_pred[:, :1]], dim=1)
+            x_pred[0, -1] = next_index

model.py
@@ -15,32 +15,30 @@ class Model(torch.nn.Module):
         self.encoder = torch.nn.Linear(embedding_size, layer_size)
         self.gru = torch.nn.GRU(layer_size, layer_size // 2, batch_first=True,
                                 bidirectional=True)
-        self.linear = torch.nn.Linear(layer_size * 2, layer_size)
+        self.linear = torch.nn.Linear(layer_size * 2, layer_size, bias=False)
         self.dropout = torch.nn.Dropout(dropout)
         self.batch_norm = torch.nn.BatchNorm1d(sequence_size)
         self.decoder = torch.nn.Linear(layer_size * sequence_size, output_size)
     
-    def forward(self, x, use_cuda=False):
+    def forward(self, x):
         x = self.embedding(x)
+        x = self.dropout(x)
         x = self.encoder(x)
-        x = self.batch_norm(x)
         x = torch.nn.functional.leaky_relu(x)
-        hidden = self.init_hidden(len(x), use_cuda)
+        x = self.dropout(x)
         x_gru = x
         for _ in range(self.num_layers):
-            x_gru, hidden = self.gru(x_gru, hidden)
-            x_gru = self.dropout(x_gru)
+            x_gru, self.hidden = self.gru(x_gru, self.hidden)
             x = torch.cat([x, x_gru], -1)
             x = self.linear(x)
             x = self.batch_norm(x)
             x = torch.nn.functional.leaky_relu(x)
+            x = self.dropout(x)
         x = x.view(len(x), -1)
         x = self.decoder(x)
         return x
 
-    def init_hidden(self, batch_size, use_cuda):
-        hidden = torch.zeros(2, batch_size, self.layer_size // 2)
-        torch.nn.init.uniform_(hidden, 0, 0.1)
-        if use_cuda:
-            return hidden.cuda()
-        return hidden
+    def init_hidden(self, batch_size, init_range=0.1):
+        weight = next(self.parameters())
+        self.hidden = weight.new_zeros(2, batch_size, self.layer_size // 2)
+        torch.nn.init.uniform_(self.hidden, 0, init_range)

train.py
@@ -45,19 +45,19 @@ def load_data(data_dir, sequence_size):
 
 
 def generate_demo(model, demo_length, text, sequence_size, chars_indices,
-                  indices_chars, use_cuda):
+                  indices_chars):
     """
-    Print demo generation on different diffusion after every epoch.
+    Print demo generation on different temperature after every epoch.
     """
     print()
     start_index = np.random.randint(len(text) - sequence_size)
-    for diffusion in [0.2, 0.3, 0.4]:
-        print(5*'-', 'Diffusion:', diffusion)
+    for temperature in [0.9, 0.8, 0.7]:
+        print(5*'-', 'Temperature:', temperature)
         sequence = text[start_index: start_index + sequence_size]
         print(5*'-', f'Generating with seed: "{sequence}"')
         stdout.write(sequence)
         for char in sample_text(model, demo_length, chars_indices,
-                                indices_chars, sequence, diffusion, use_cuda):
+                                indices_chars, sequence, temperature):
             stdout.write(char)
             stdout.flush()
         print()    
@@ -69,11 +69,11 @@ def parse_args():
     parser.add_argument('--sequence_size', type=int, required=True)
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--num_layers', type=int, default=3)
-    parser.add_argument('--layer_size', type=int, default=64)
+    parser.add_argument('--layer_size', type=int, default=128)
     parser.add_argument('--embedding_size', type=int, default=16)
     parser.add_argument('--learning_rate', type=float, default=0.001)
-    parser.add_argument('--regularizer_rate', type=float, default=0.005)
-    parser.add_argument('--dropout', type=float, default=0.05)
+    parser.add_argument('--weight_decay', type=float, default=0.005)
+    parser.add_argument('--dropout', type=float, default=0.1)
     parser.add_argument('--data_dir', type=str, default='data')
     parser.add_argument('--model_dir', type=str, default='checkpoints')
     parser.add_argument('--model_name', type=str, default='model')
@@ -96,22 +96,26 @@ if __name__ == '__main__':
         model = model.cuda()
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.RMSprop(model.parameters(), args.learning_rate,
-                                    weight_decay=args.regularizer_rate)
-    dataloader = torch.utils.data.DataLoader(dataset, args.batch_size)
+                                    weight_decay=args.weight_decay)
+    dataloader = torch.utils.data.DataLoader(dataset, args.batch_size,
+                                             shuffle=True)
 
     for epoch in range(1, args.epochs + 1):
         progress = tqdm(dataloader)
-        for i, (x_batch, y_batch) in enumerate(progress):
+        losses = []
+        for x_batch, y_batch in progress:
             if use_cuda:
                 x_batch, y_batch = x_batch.cuda(), y_batch.cuda()
-            y_preds = model(x_batch, use_cuda)
+            model.init_hidden(len(x_batch))
+            y_preds = model(x_batch)
             loss = criterion(y_preds, y_batch)
-            progress.set_description('Loss: {:.4f}'.format(loss.item()))
+            losses.append(loss.item())
+            progress.set_description('Loss: {:.4f}'.format(np.mean(losses)))
             model.zero_grad()
             loss.backward()
             optimizer.step()
         generate_demo(model, args.demo_length, text, args.sequence_size,
-                      chars_indices, indices_chars, use_cuda)
+                      chars_indices, indices_chars)
         model_name = f'{args.model_name}_{epoch}.pt'
         print(f'"{model_name}" saved')
         torch.save(model, path.join(args.model_dir, model_name))